0883263c495bb0baec4d80552775d7ad74c3d8ad
samuel.behrmann@yahoo.de <samuel.behrmann@yahoo.de>
2024-04-04 08:32:48 +0200

    first rudimentary version of reading dxf files

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ drawButton.addEventListener('click', () => {
 
 fileInput.addEventListener('change', handleFileSelect);
 
-function handleFileSelect(event) {
+function handleFileSelect(event: any) {
     const file = event.target.files[0];
     if (!file) return;
 
@@ -23,10 +23,47 @@ function handleFileSelect(event) {
             console.warn("Result was not a string!");
             return;
         }
+        ReadDXF(event.target.result, "ENTITIES", "LINE", "10,20,30");
         const h = new Helper(event.target.result);
-        numberOfEntities.innerHTML = h.denormalised?.length.toString()
+        numberOfEntities.innerHTML = h.denormalised.length.toString()
         svgContainer.innerHTML = h.toSVG()
     };
 
     reader.readAsBinaryString(file)
+}
+
+function ReadDXF(dxfFileAsStr:String, strSection:String, strObject:String, strCodeList:String) {
+    let codes = dxfFileAsStr.split("\n").map((line) => line.trim());
+    let result: [string, string][] = [];
+
+    console.log('reading ' + codes );
+    while (codes.length > 0 && codes[0] !== "EOF") {
+        if (codes[0] === "0" && codes[1] === "SECTION") {
+            console.log('section found');
+            codes = codes.slice(2);
+            if (codes[1] === strSection) {
+                console.log('section is correct');
+                codes = codes.slice(2);
+                while (codes[1] != "ENDSEC") {
+                    if (codes[0] === "0" && codes[1] === strObject) {
+                        console.log('object found ' + codes[1]);
+                        codes = codes.slice(2);
+                        while (codes[0] != "0") {
+                            console.log('codes: ' + codes);
+                            if (strCodeList.includes(codes[0])) {
+                                console.log('code found ' + codes[0] + " " + codes[1]);
+                                result.push([codes[0], codes[1]] );
+                            }
+                            codes = codes.slice(2);
+                        }
+                    } else {
+                        codes = codes.slice(2);
+                    }
+                }
+            }
+        } else {
+            codes = codes.slice(2);
+        }
+    }
+    console.log("result: " + result );
 }
